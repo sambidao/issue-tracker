edccc77e896ff5df6ff3feb0e2827c2153b772b8
sambidao <99098880+sambidao@users.noreply.github.com>
2024-03-01 16:50:46 +0000

    Navbar created video 4

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,13 +1,19 @@
 import Link from 'next/link'
 import React from 'react'
+import { RiBug2Fill } from "react-icons/ri";
+
+
+const links = [
+  {label: 'Dashboar', href:'/'},
+  {label: 'Issue', href:'/'}
+]
 
 const NavBar = () => {
   return (
-    <nav className='flex bg-white border-gray-200'>
-        <Link href='/'>Logo</Link>
-        <ul className='flex '>
-            <li>Dashboard</li>
-            <li>Issue</li>
+    <nav className='flex space-x-6 border-b p-5 font-bold'>
+        <Link className='mt-1' href='/'><RiBug2Fill /></Link>
+        <ul className='flex space-x-6'>
+            {links.map(link => <li key={link.href} className='text-zinc-500 hover:text-zinc-800'><Link href={link.href}>{link.label}</Link></li>)}
         </ul>
     </nav>
   )
